baeb761bf1b7a3c23e2808585d0111b490eaeaea
InnaKovrizhenko <114367918+InnaKovrizhenko@users.noreply.github.com>
2023-05-07 00:26:19 +0300

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react'
+import { ContactForm } from './ContactForm/ContactForm'
+import { Filter } from './Filter/Filter'
+import { ContactList } from './ContactList/ContactList'
+import { Titleh1, Titleh2, Phonebook } from './App.styled'
+
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: ''
+  }
+
+  addContact = (contact) => {
+    this.setState (({ contacts }) => ({
+      contacts: [...contacts, contact],
+    }))
+  }
+
+  changeFilter = (el) => {
+    this.setState(
+      {filter: el.target.value}
+    )
+  }
+
+  handleFilterContact = () => {
+    return this.state.contacts.filter(contact => contact.name.toLowerCase()
+    .includes(this.state.filter.toLowerCase().trim()));
+  }
+
+  deleteContact = (contactId) => {
+      this.setState(prevState => {
+        return {
+          contacts: prevState.contacts.filter((contact) => contact.id !== contactId)
+        }
+      })
+  }
+
+  render() {
+    const state = this.state;
+    return (
+      <Phonebook>
+        <Titleh1>Phonebook</Titleh1>
+        <ContactForm addContact={this.addContact} contacts={state.contacts} />
+        <Titleh2>Contacts</Titleh2>
+        <Filter value={state.filter} onChange={this.changeFilter}/>
+        <ContactList visibleListContacts={this.handleFilterContact()} deleteContact={this.deleteContact} />
+      </Phonebook>
+    )
+  }
+}
